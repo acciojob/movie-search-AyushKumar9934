bd992d5b3aed46b09475f3e07d2fbd8f9af03626
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-06-15 16:37:51 +0530

    p

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [current, setCurrent] = useState("");
+  const [allData, setAllData] = useState([]);
+
+  async function handleSearch() {
+    const res = await fetch(`http://www.omdbapi.com/?s=${current}&apikey=f732e5c0`);
+    const data = await res.json();
+    if (data.Search) {
+      setAllData(data.Search);
+    } else {
+      setAllData([]);
+    }
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input value={current} placeholder="Search Movie by title" onChange={(event) => { setCurrent(event.target.value) }} />
+      <span><button onClick={handleSearch}>Search</button></span>
+      {
+        allData.length === 0 ? <p className="error">Invalid movie name. Please try again.</p> :
+          allData.map((movie) => {
+            return (
+              <div key={movie.imdbID}>
+                <ul><li>{movie.Title} ({movie.Year})</li></ul>
+                <img src={movie.Poster} alt="movie poster"/>
+              </div>
+            );
+          })
+      }
     </div>
   )
 }
 
-export default App
+export default App;
